3e35aaf316a44a4e8d5a71ae041c96728b251fca
Zyss7 <sebastianv524@gmail.com>
2022-02-20 19:56:59 -0500

    ADD: dependencias

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-unused-vars */
 /** @type {import('next').NextConfig} */
 module.exports = {
+  swcMinify: true,
   webpack5: true,
   eslint: {
     ignoreDuringBuilds: true,
@@ -9,5 +11,21 @@ module.exports = {
   },
   typescript: {
     ignoreBuildErrors: true,
+    forceConsistentCasingInFileNames: false,
+  },
+  experimental: {
+    styledComponents: true,
+  },
+  reactStrictMode: true,
+  compress: true,
+  webpack: (config, _) => {
+    config.resolve.extensions = [...config.resolve.extensions, ...['.gql', '.graphql']];
+    config.module.rules.push({
+      test: /\.(graphql|gql)$/,
+      exclude: /node_modules/,
+      loader: 'graphql-tag/loader',
+    });
+
+    return config;
   },
 };
